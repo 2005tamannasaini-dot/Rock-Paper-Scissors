4f14483c05d9bf87b0cfd499fa157a2f064403b4
2005tamannasaini-dot <2005tamannasaini@email.com>
2026-08-05 12:24:52 +0530

    Added game result logic

--- Rock PaperScissors.py
+++ Rock PaperScissors.py
@@ -7,8 +7,20 @@ print("=== Welcome to Rock Paper Scissors Game. ===")
 choose = ["rock", "paper", "scissors"]
 computer_choice = random.choice(choose)
 
-user_input = input("please choose(Rock/Paper/Scissors) :").lower()
 
-print("your choice:", user_input)
+user_choice = input("please choose(Rock/Paper/Scissors) :").lower()
+
+print("your choice:", user_choice)
 print("Computer choice:", computer_choice )
+
+if computer_choice == user_choice:
+    print("It's a draw.")
+
+elif ((computer_choice == "rock" and user_choice == "paper")
+       or (computer_choice == "paper" and user_choice == "scissors") 
+      or (computer_choice == "scissors" and  user_choice == "rock")):
+    print("you win!.")
+
+else:
+    print("computer wins!.")    
 
