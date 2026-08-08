11a5395955af308c6737a7e15517a6347224a715
2005tamannasaini-dot <2005tamannasaini@email.com>
2026-08-08 11:45:53 +0530

    Added input validation

--- Rock PaperScissors.py
+++ Rock PaperScissors.py
@@ -10,17 +10,21 @@ computer_choice = random.choice(choose)
 
 user_choice = input("please choose(Rock/Paper/Scissors) :").lower()
 
-print("your choice:", user_choice)
-print("Computer choice:", computer_choice )
-
-if computer_choice == user_choice:
-    print("It's a draw.")
-
-elif ((computer_choice == "rock" and user_choice == "paper")
-       or (computer_choice == "paper" and user_choice == "scissors") 
-      or (computer_choice == "scissors" and  user_choice == "rock")):
-    print("you win!.")
-
+if user_choice not in choose:
+    print("Invalid choice! Please choose rock, paper, or scissors.")
+    
 else:
-    print("computer wins!.")    
+    print("your choice:", user_choice)
+    print("Computer choice:", computer_choice )
+
+    if computer_choice == user_choice:
+        print("It's a draw.")
+
+    elif ((computer_choice == "rock" and user_choice == "paper")
+        or (computer_choice == "paper" and user_choice == "scissors") 
+        or (computer_choice == "scissors" and  user_choice == "rock")):
+        print("you win!.")
+
+    else:
+        print("computer wins!.")    
 
