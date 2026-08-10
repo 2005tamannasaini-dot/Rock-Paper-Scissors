4e32c7bc712db810cbf4ae11ab1d3be8345800ab
2005tamannasaini-dot <2005tamannasaini@email.com>
2026-08-10 12:36:11 +0530

    Added restart option

--- Rock PaperScissors.py
+++ Rock PaperScissors.py
@@ -5,34 +5,48 @@ import random
 print("=== Welcome to Rock Paper Scissors Game. ===")
 
 choose = ["rock", "paper", "scissors"]
-computer_choice = random.choice(choose)
 
 computer_score = 0
 user_score = 0
 
-user_choice = input("please choose(Rock/Paper/Scissors) :").lower()
+while True:
 
-if user_choice not in choose:
-    print("Invalid choice! Please choose rock, paper, or scissors.")
-    
-else:
-    print("your choice:", user_choice)
-    print("Computer choice:", computer_choice )
+    computer_choice = random.choice(choose)
+    user_choice = input("please choose(Rock/Paper/Scissors) :").lower()
 
-    if computer_choice == user_choice:
-        print("It's a draw.")
-
-    elif ((computer_choice == "rock" and user_choice == "paper")
-        or (computer_choice == "paper" and user_choice == "scissors") 
-        or (computer_choice == "scissors" and  user_choice == "rock")):
-        print("you win!.")
-        user_score += 1
-
+    if user_choice not in choose:
+        print("Invalid choice! Please choose rock, paper, or scissors.")
+        
     else:
-        print("computer wins!.") 
-        computer_score += 1 
+        print("your choice:", user_choice)
+        print("Computer choice:", computer_choice )
 
-    print("your score:", user_score)
-    print("computer score:", computer_score)      
-      
+        if computer_choice == user_choice:
+            print("It's a draw.")
+
+        elif ((computer_choice == "rock" and user_choice == "paper")
+            or (computer_choice == "paper" and user_choice == "scissors") 
+            or (computer_choice == "scissors" and  user_choice == "rock")):
+            print("you win!.")
+            user_score += 1
+
+        else:
+            print("computer wins!.") 
+            computer_score += 1 
+
+        
+        while True:
+            choice = input("Play Again?(yes/no):").lower()
+            if choice == "yes":
+                break
+
+            elif choice =="no":
+                print("Game Over!")
+                print("your score:", user_score)
+                print("computer score:", computer_score)  
+                quit()
+
+            else:
+                print("please! choose only yes or no.")            
+            
 
